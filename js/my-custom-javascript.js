ee97dcfc0587eb2308f8496a06e6dac091f94780
Jacob-Valeriano <jacob.k.valeriano@gmail.com>
2021-02-02 13:51:08 -0600

    Class Selectors Completed

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -5,14 +5,16 @@ $(document).ready(function() {
 
 
 
-        var content1 = $('.heading').css('color', 'red');
-
-
-        var content2 = $('#p2').css('color', 'blue');
-
-
-        var content3 = $('#p3').css('color', 'gold');
+        // var content1 = $('.heading').css('color', 'red');
+        //
+        //
+        // var content2 = $('#p2').css('color', 'blue');
+        //
+        //
+        // var content3 = $('#p3').css('color', 'gold');
 
     // INSERT JAVASCRIPT CODE SAMPLE HERE
+
+        var border = $('.codeup').css({'border': '1px solid red'})
 
 });
